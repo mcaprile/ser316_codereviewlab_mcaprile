2848c5865e1ede5d40a13e1c91ad20d62214b5d9
Michelle Capriles-Escobedo <caprilem@gmail.com>
2017-04-07 08:29:32 -0700

    changed literals to constants
    Removed the use of literals and made them constants.

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -3,6 +3,10 @@ package banking.primitive.core;
 public class Checking extends Account {
 
 	private static final long serialVersionUID = 11L;
+	final float OVERDRAWN_LIMIT = -100.0f;
+	final int MAX_WITHDRAWALS = 10;
+	final float WITHDRAWAL_FEE = 2.0f;
+	final float EMPTY = 0.0f;
 	private int numWithdraws = 0;
 	
 	private Checking(String name) {
@@ -22,9 +26,9 @@ public class Checking extends Account {
 	 * @param float is the deposit amount
 	 */
 	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
+		if (getState() != State.CLOSED && amount > EMPTY) {
 			balance = balance + amount;
-			if (balance >= 0.0f) {
+			if (balance >= EMPTY) {
 				setState(State.OPEN);
 			}
 			return true;
@@ -37,14 +41,14 @@ public class Checking extends Account {
 	 * continue to withdraw an overdrawn account until the balance is below -$100
 	 */
 	public boolean withdraw(float amount) {
-		if (amount > 0.0f) {		
+		if (amount > EMPTY) {		
 			// KG: incorrect, last balance check should be >=
-			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > -100.0f)) {
+			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > OVERDRAWN_LIMIT)) {
 				balance = balance - amount;
 				numWithdraws++;
-				if (numWithdraws > 10)
-					balance = balance - 2.0f;
-				if (balance < 0.0f) {
+				if (numWithdraws > MAX_WITHDRAWALS)
+					balance = balance - WITHDRAWAL_FEE;
+				if (balance < EMPTY) {
 					setState(State.OVERDRAWN);
 				}
 				return true;
